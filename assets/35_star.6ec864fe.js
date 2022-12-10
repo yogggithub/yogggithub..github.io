f0fcac619992a5b536526917c68bd4604444fc7c
yogggithub <35437419+yogggithub@users.noreply.github.com>
2022-12-10 02:02:30 +0000

    Deploying to gh-pages from @ yogggithub/yogggithub.github.io@6aece50954ff4fa5e6a2d53fd707ef317942acf3 🚀

---
+++ assets/35_star.6ec864fe.js
@@ -0,0 +1,1 @@
+const s="/assets/icon/35_star.png";export{s as _};
---
+++ assets/hczs.html.8ec585a6.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as a,c as r,e as c}from"./app.7d6088e7.js";const n={},t=c(`<h1 id="红尘之殇" tabindex="-1"><a class="header-anchor" href="#红尘之殇" aria-hidden="true">#</a> 红尘之殇</h1><p></p><h2 id="楔子" tabindex="-1"><a class="header-anchor" href="#楔子" aria-hidden="true">#</a> 楔子</h2><pre><code>前言
+开本新书，算是长篇都市类型吧。构思了很长时间，写的也挺挣扎，挺迷茫的，不知道能不能吃这碗饭。先更着吧，大纲写完了，也一直再完善细节，反正就是绿帽凌辱类的，书里出现的美女都跑不了。希望大家多多支持吧，多谢了。  
+
+时光是一个巨大的黑洞，这个世界上所有人的秘密和疼痛都悉数丢进这个洞里，这些人可以选择停驻，也可以选择离开。最终所有原本以为不能愈合的伤口，都在洪荒里被冲刷揉搓，深知汪洋的沉静，将一切鲜血和眼泪覆盖。  
+</code></pre><p></p><h2 id="第一章-回京" tabindex="-1"><a class="header-anchor" href="#第一章-回京" aria-hidden="true">#</a> 第一章：回京</h2><pre><code>曼哈顿，一家高档酒店。  
+房间是一排整齐的落地窗，透过玻璃可以看到是一个高级套房的格局，设施齐全，装修淡雅。
+</code></pre>`,7),d=[t];function h(o,s){return a(),r("div",null,d)}const p=e(n,[["render",h],["__file","hczs.html.vue"]]);export{p as default};
